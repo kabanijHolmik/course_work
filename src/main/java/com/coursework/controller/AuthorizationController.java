7d8a907effd520d199f58aea905098505a30b60b
kabanijHolmik <kosenkov324@gmail.com>
2023-11-23 17:50:25 +0300

    добавлены методы регистрации новго пользователя

--- src/main/java/com/coursework/controller/AuthorizationController.java
+++ src/main/java/com/coursework/controller/AuthorizationController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,7 +22,7 @@ public class AuthorizationController {
 
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password, Model model) {
-        User user = new User(username, password, null); // Создаем объект User с логином и паролем
+        User user = new User(username, password, null);
 
         if (userService.checkUser(user)) {
             model.addAttribute("message", "Авторизация успешна");
@@ -30,5 +31,30 @@ public class AuthorizationController {
             model.addAttribute("message", "Неверный логин или пароль");
             return "authorization";
         }
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUser(@ModelAttribute("user") User user, Model model) {
+
+        if (user.getLogin() == null || user.getPassword() == null || user.getLogin().isEmpty() || user.getPassword().isEmpty()) {
+            model.addAttribute("error", "Пожалуйста, заполните все поля");
+            return "registration";
+        }
+        if (userService.hasSameUsername(user)){
+            model.addAttribute("error", "Имя пользователя занято");
+            return  "registration";
+        }
+
+        // Проведем регистрацию пользователя
+        userService.saveUser(user);
+
+        // Перенаправим пользователя на страницу входа
+        return "redirect:/login";
     }
 }
